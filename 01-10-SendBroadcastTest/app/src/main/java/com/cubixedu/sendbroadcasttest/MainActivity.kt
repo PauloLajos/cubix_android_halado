bbda33aed2a6bf42d3b11e34751d6d2036776025
Pauló Lajos <dev@paulolajos.hu>
2023-06-27 06:49:02 +0200

    01. -10- SendBroadcastTest - send broadcast

--- 01-10-SendBroadcastTest/app/src/main/java/com/cubixedu/sendbroadcasttest/MainActivity.kt
+++ 01-10-SendBroadcastTest/app/src/main/java/com/cubixedu/sendbroadcasttest/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.cubixedu.sendbroadcasttest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cubixedu.sendbroadcasttest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        mainBinding.btnSend.setOnClickListener {
+            //sendBroadcast(Intent("com.cubixedu.broadcastreceivertest.NOTIFY"))
+            var myIntent = Intent("com.cubixedu.broadcastreceivertest.NOTIFY")
+            myIntent.putExtra("KEY_DATA", "HELLO")
+            sendBroadcast(myIntent)
+        }
     }
 }
