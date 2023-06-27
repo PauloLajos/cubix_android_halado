b712b69a25f8218196c0452e67dd0b298b1ec098
Pauló Lajos <dev@paulolajos.hu>
2023-06-27 06:26:49 +0200

    01. -10- BroadcastReceiverTest - OutCallReceiver, start activity

--- 01-10-BroadcastReceiverTest/app/src/main/java/com/cubixedu/broadcastreceivertest/OutCallReceiver.kt
+++ 01-10-BroadcastReceiverTest/app/src/main/java/com/cubixedu/broadcastreceivertest/OutCallReceiver.kt
@@ -9,5 +9,11 @@ class OutCallReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
         val outNumber = intent?.getStringExtra(Intent.EXTRA_PHONE_NUMBER)
         Toast.makeText(context, outNumber, Toast.LENGTH_LONG).show()
+
+        //this.resultData = "1001"
+
+        var intent = Intent(context, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        context?.startActivity(intent)
     }
 }
