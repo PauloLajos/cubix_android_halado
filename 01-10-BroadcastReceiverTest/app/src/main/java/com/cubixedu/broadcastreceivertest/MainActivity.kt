46ed3c667584b29c5b990cc1b9664ab34961905e
Pauló Lajos <dev@paulolajos.hu>
2023-06-27 06:14:01 +0200

    01. -10- BroadcastReceiverTest - uses permissions, receivers, not work

--- 01-10-BroadcastReceiverTest/app/src/main/java/com/cubixedu/broadcastreceivertest/MainActivity.kt
+++ 01-10-BroadcastReceiverTest/app/src/main/java/com/cubixedu/broadcastreceivertest/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.cubixedu.broadcastreceivertest
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+import com.cubixedu.broadcastreceivertest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        requestNeededPermission()
+    }
+
+    private fun requestNeededPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.PROCESS_OUTGOING_CALLS) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECEIVE_BOOT_COMPLETED) != PackageManager.PERMISSION_GRANTED
+            || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.FOREGROUND_SERVICE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.RECEIVE_SMS,
+                    Manifest.permission.PROCESS_OUTGOING_CALLS,
+                    Manifest.permission.RECEIVE_BOOT_COMPLETED,
+                    Manifest.permission.FOREGROUND_SERVICE
+                ),
+                101)
+        } else {
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            101 -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty()
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[2] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[3] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this@MainActivity, "All perm granted", Toast.LENGTH_SHORT).show()
+
+
+                } else {
+                    Toast.makeText(this@MainActivity,
+                        "Permissions NOT granted", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
     }
 }
