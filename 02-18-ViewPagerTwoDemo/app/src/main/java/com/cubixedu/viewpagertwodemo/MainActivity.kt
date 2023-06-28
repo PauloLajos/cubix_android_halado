a7c4da9cdbada5c2faffecc23762b3ebd57d20fa
Pauló Lajos <dev@paulolajos.hu>
2023-06-28 09:02:21 +0200

    02. -18- ViewPagerTwoDemo - OnPageChangeCallback

--- 02-18-ViewPagerTwoDemo/app/src/main/java/com/cubixedu/viewpagertwodemo/MainActivity.kt
+++ 02-18-ViewPagerTwoDemo/app/src/main/java/com/cubixedu/viewpagertwodemo/MainActivity.kt
@@ -2,11 +2,20 @@ package com.cubixedu.viewpagertwodemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.viewpager2.widget.ViewPager2
 import com.cubixedu.viewpagertwodemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
+
+    private var pageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            Toast.makeText(this@MainActivity, "Selected position: $position",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +24,15 @@ class MainActivity : AppCompatActivity() {
 
         val fragmentStatePagerAdapter = MyFragmentStatePageAdapter(this, 2)
         mainBinding.mainViewPager.adapter = fragmentStatePagerAdapter
+
+        //binding.mainViewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
+
+        mainBinding.mainViewPager.registerOnPageChangeCallback(pageChangeCallback)
+
+    }
+
+    override fun onDestroy() {
+        mainBinding.mainViewPager.unregisterOnPageChangeCallback(pageChangeCallback)
+        super.onDestroy()
     }
 }
