4b8ee09b5bfe6e9e952daab2a77981f0477fa4aa
Pauló Lajos <dev@paulolajos.hu>
2023-06-28 08:49:01 +0200

    02. -18- ViewPagerTwoDemo - init

---
+++ 02-18-ViewPagerTwoDemo/app/src/main/java/com/cubixedu/viewpagertwodemo/FragmentPageTwo.kt
@@ -0,0 +1,23 @@
+package com.cubixedu.viewpagertwodemo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentPageTwo : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_page_two, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            FragmentPageTwo()
+    }
+}
