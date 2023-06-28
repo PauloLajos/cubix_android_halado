c1389173c69acf23f5f81eb2b01dfe459b20262f
Pauló Lajos <dev@paulolajos.hu>
2023-06-28 08:57:54 +0200

    02. -18- ViewPagerTwoDemo - MyFragmentStatePageAdapter

--- 02-18-ViewPagerTwoDemo/app/src/main/java/com/cubixedu/viewpagertwodemo/MainActivity.kt
+++ 02-18-ViewPagerTwoDemo/app/src/main/java/com/cubixedu/viewpagertwodemo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.cubixedu.viewpagertwodemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cubixedu.viewpagertwodemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        val fragmentStatePagerAdapter = MyFragmentStatePageAdapter(this, 2)
+        mainBinding.mainViewPager.adapter = fragmentStatePagerAdapter
     }
 }
