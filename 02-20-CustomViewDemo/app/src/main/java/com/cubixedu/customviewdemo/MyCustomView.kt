927069d48e87341bb7d48552f8214579d55dcc4e
Pauló Lajos <dev@paulolajos.hu>
2023-06-29 04:56:30 +0200

    02. -20- CustomViewDemo - custom attributes, obtainStyledAttributes

--- 02-20-CustomViewDemo/app/src/main/java/com/cubixedu/customviewdemo/MyCustomView.kt
+++ 02-20-CustomViewDemo/app/src/main/java/com/cubixedu/customviewdemo/MyCustomView.kt
@@ -5,21 +5,53 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
 
-class MyCustomView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+class MyCustomView(context: Context, attrs: AttributeSet?) : AppCompatButton(context, attrs) {
 
-    private var paintBg: Paint = Paint()
+    private var paintText: Paint = Paint()
+    private var click = 0
 
     init {
-        paintBg.color = Color.BLUE
-        paintBg.style = Paint.Style.FILL
+        paintText.color = Color.BLUE
+        paintText.style = Paint.Style.FILL_AND_STROKE
+        paintText.textSize = 60f
+
+        val attr = context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CountColor,
+            0, 0
+        )
+        var color: Int
+        try {
+            color = attr.getColor(R.styleable.CountColor_countcolor, Color.RED)
+            paintText.color = color
+        } finally {
+            // release the TypedArray so that it can be reused.
+            attr.recycle()
+        }
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        canvas?.drawRect(0f, 0f, width.toFloat(), height.toFloat(),paintBg)
+        canvas?.drawText(
+            "$click",
+            width-120f,
+            120f,
+            paintText
+        )
     }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event!!.action == MotionEvent.ACTION_DOWN) {
+            click++
+            invalidate()
+        }
+
+        return super.onTouchEvent(event)
+    }
 }
