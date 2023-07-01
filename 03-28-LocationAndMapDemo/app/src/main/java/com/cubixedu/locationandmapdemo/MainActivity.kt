0975029e465aa7b0b0b5f40801d9531f40157844
Pauló Lajos <dev@paulolajos.hu>
2023-07-01 13:46:58 +0200

    03. -28- LocationAndMapDemo - permissions

--- 03-28-LocationAndMapDemo/app/src/main/java/com/cubixedu/locationandmapdemo/MainActivity.kt
+++ 03-28-LocationAndMapDemo/app/src/main/java/com/cubixedu/locationandmapdemo/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.cubixedu.locationandmapdemo
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.cubixedu.locationandmapdemo.databinding.ActivityMainBinding
+import android.Manifest
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
@@ -12,5 +17,54 @@ class MainActivity : AppCompatActivity() {
 
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+
+        requestNeededPermission()
+    }
+
+    private fun requestNeededPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                101
+            )
+        } else {
+            // we have the permission
+            handleLocationStart()
+        }
+    }
+
+    private fun handleLocationStart() {
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            101 -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "ACCESS_FINE_LOCATION perm granted", Toast.LENGTH_SHORT)
+                        .show()
+
+                    handleLocationStart()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "ACCESS_FINE_LOCATION perm NOT granted", Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return
+            }
+        }
     }
 }
