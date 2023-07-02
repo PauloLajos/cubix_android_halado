5e6b4efcb941d72dc3bf08118d5529e26b733b97
Pauló Lajos <dev@paulolajos.hu>
2023-07-02 07:37:56 +0200

    Practice - GoogleMapsDemo, secret hide api key

---
+++ practice/GoogleMapsDemo/app/src/main/java/com/practice/googlemapsdemo/MapsActivity.kt
@@ -0,0 +1,49 @@
+package com.practice.googlemapsdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.practice.googlemapsdemo.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Gyula, Hu and move the camera
+        // 46.646165, 21.271715
+        val gyula = LatLng(46.646165, 21.271715)
+        mMap.addMarker(MarkerOptions().position(gyula).title("Marker in Gyula"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(gyula))
+    }
+}
