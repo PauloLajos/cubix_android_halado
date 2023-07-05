38fbbc414efebb1a0c5046a3b9814ca5d0a02936
Pauló Lajos <dev@paulolajos.hu>
2023-07-05 19:37:07 +0200

    03. -37- SunDirectionOnMap - calculateSunDirection, bad

--- 03-37-SunDirectionOnMap/app/src/main/java/com/cubixedu/sundirectiononmap/MainActivity.kt
+++ 03-37-SunDirectionOnMap/app/src/main/java/com/cubixedu/sundirectiononmap/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.cubixedu.sundirectiononmap
 
 import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentActivity
@@ -11,14 +10,24 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.OnMapReadyCallback
 import android.Manifest
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polygon
+import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import java.util.Calendar
+import kotlin.math.acos
+import kotlin.math.cos
+import kotlin.math.sin
 
 class MainActivity : FragmentActivity(), OnMapReadyCallback {
+
+    private val cameraZoom: Float = 18f
 
     private lateinit var mainBinding: ActivityMainBinding
 
@@ -26,12 +35,15 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val permissionCode = 101
 
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
+        mainBinding.tvHello.text = "Search GPS..."
         fusedLocationProviderClient =  LocationServices.getFusedLocationProviderClient(this@MainActivity)
         fetchLocation()
 
@@ -61,11 +73,22 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+
         val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+        //val latLng = LatLng(49.099288,19.029719)
         val markerOptions = MarkerOptions().position(latLng).title("I am here!")
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 24f))
-        googleMap.addMarker(markerOptions)
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, cameraZoom))
+        mMap.addMarker(markerOptions)
+
+        mainBinding.tvHello.text = "Place find"
+
+        drawLine(latLng)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
@@ -77,5 +100,53 @@ class MainActivity : FragmentActivity(), OnMapReadyCallback {
                 fetchLocation()
             }
         }
+    }
+
+    private fun drawLine(latLng: LatLng) {
+
+        val azimuthAngle = calculateSunDirection()
+
+        mainBinding.tvHello.text = "azimut = %.2f".format(azimuthAngle)
+
+        val lineEndX = 0.0005 * sin(Math.toRadians(azimuthAngle))
+        val lineEndY = 0.0005 * cos(Math.toRadians(azimuthAngle))
+
+        val polyLineOpts = PolylineOptions().add(
+            LatLng(latLng.latitude, latLng.longitude),
+            LatLng(latLng.latitude + lineEndX, latLng.longitude + lineEndY)
+        )
+        val polyline = mMap.addPolyline(polyLineOpts)
+        polyline.color = Color.GREEN
+    }
+
+    private fun calculateSunDirection(): Double {
+        val calendar = Calendar.getInstance()
+        val dayOfYear = calendar.get(Calendar.DAY_OF_YEAR)
+        val latitude = currentLocation.latitude
+        val longitude = currentLocation.longitude
+        // Time zone offset in hours
+        val timeZoneOffset = calendar.timeZone.rawOffset / (1000 * 60 * 60)
+        val declination = 23.45 * sin(Math.toRadians((360.toDouble() / 365) * (dayOfYear - 81)))
+
+        val hourAngle = (15 * (12 - (12 + timeZoneOffset) - (longitude / 15)))
+        val cosHA = cos(Math.toRadians(hourAngle))
+        // val sinHA = sin(Math.toRadians(hourAngle))
+        val cosDec = cos(Math.toRadians(declination))
+        val sinDec = sin(Math.toRadians(declination))
+
+        val zenithAngle = Math.toDegrees(
+            acos(
+                sinDec * sin(Math.toRadians(latitude)) +
+                        cosDec * cos(Math.toRadians(latitude)) * cosHA
+            )
+        )
+        val azimuthAngle = Math.toDegrees(
+            acos(
+                (sinDec * cos(Math.toRadians(latitude)) - cosDec * sin(Math.toRadians(latitude)) * cosHA) /
+                        sin(Math.toRadians(zenithAngle))
+            )
+        )
+
+        return if (hourAngle > 0) azimuthAngle else 360 - azimuthAngle
     }
 }
