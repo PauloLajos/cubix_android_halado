6b4882b7b69af5199eb6deab96cdfc95dbc76536
Pauló Lajos <dev@paulolajos.hu>
2023-07-05 17:46:59 +0200

    Practice - ShowCurrentPositionOnTheMap

---
+++ practice/ShowCurrentPositionOnTheMap/app/src/main/java/com/cubixedu/showcurrentpositiononthemap/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.cubixedu.showcurrentpositiononthemap
+
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.FragmentActivity
+import com.cubixedu.showcurrentpositiononthemap.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import android.Manifest
+
+class MainActivity : FragmentActivity(), OnMapReadyCallback {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    private lateinit var currentLocation: Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val permissionCode = 101
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        fusedLocationProviderClient =  LocationServices.getFusedLocationProviderClient(this@MainActivity)
+        fetchLocation()
+
+    }
+
+    private fun fetchLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), permissionCode)
+            return
+        }
+        val task = fusedLocationProviderClient.lastLocation
+        task.addOnSuccessListener { location ->
+            if (location != null) {
+                currentLocation = location
+                Toast.makeText(applicationContext, currentLocation.latitude.toString() + "" +
+                        currentLocation.longitude, Toast.LENGTH_SHORT).show()
+                val supportMapFragment = (supportFragmentManager.findFragmentById(R.id.fragmentMap) as
+                        SupportMapFragment?)!!
+                supportMapFragment.getMapAsync(this@MainActivity)
+            }
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
+        val markerOptions = MarkerOptions().position(latLng).title("I am here!")
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLng(latLng))
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5f))
+        googleMap?.addMarker(markerOptions)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            permissionCode -> if (grantResults.isNotEmpty() && grantResults[0] ==
+            PackageManager.PERMISSION_GRANTED) {
+                fetchLocation()
+            }
+        }
+    }
+}
