8520adfc2cc8344ef8dc65598db62d709a58aa04
Pauló Lajos <dev@paulolajos.hu>
2023-07-22 10:20:02 +0200

    05. -55- GoodBuyDemo - restart

--- 05-55-GoodBuyDemo/app/src/main/java/com/cubix/goodbuydemo/data/AdsData.kt
+++
@@ -1,9 +0,0 @@
-package com.cubix.goodbuydemo.data
-
-data class AdsData(
-    var uid: String = "",
-    var author: String = "",
-    var title: String = "",
-    var body: String = "",
-    var imgUrl: String = ""
-)
