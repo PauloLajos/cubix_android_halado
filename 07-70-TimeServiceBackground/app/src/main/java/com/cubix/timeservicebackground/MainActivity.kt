9d47925e68f48f63954a003bbda933a5762bf4a7
Pauló Lajos <dev@paulolajos.hu>
2023-07-23 18:35:07 +0200

    07. -70- TimeServiceBackground - TimeService

--- 07-70-TimeServiceBackground/app/src/main/java/com/cubix/timeservicebackground/MainActivity.kt
+++ 07-70-TimeServiceBackground/app/src/main/java/com/cubix/timeservicebackground/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cubix.timeservicebackground
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cubix.timeservicebackground.databinding.ActivityMainBinding
@@ -14,5 +15,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val intentTimeService = Intent(this, TimeService::class.java)
+        binding.btnStart.setOnClickListener {
+            startService(intentTimeService)
+            binding.btnStart.isEnabled = false
+            binding.btnStop.isEnabled = true
+        }
+        binding.btnStop.setOnClickListener {
+            stopService(intentTimeService)
+            binding.btnStart.isEnabled = true
+            binding.btnStop.isEnabled = false
+        }
     }
 }
