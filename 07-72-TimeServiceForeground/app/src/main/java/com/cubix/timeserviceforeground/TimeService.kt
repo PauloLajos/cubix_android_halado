da19d7e6895bc4372289fa99f862801708a91f43
Pauló Lajos <dev@paulolajos.hu>
2023-07-24 03:53:33 +0200

    07. -77- TimeServiceForeground - copy of t.s.background

---
+++ 07-72-TimeServiceForeground/app/src/main/java/com/cubix/timeserviceforeground/TimeService.kt
@@ -0,0 +1,59 @@
+package com.cubix.timeserviceforeground
+
+import android.app.Service
+import android.content.Intent
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+class TimeService : Service() {
+
+    var serviceEnabled = false
+
+    inner class TimeThread : Thread() {
+        override fun run() {
+
+            // access to MainActivity from service
+            val handlerMain = Handler(Looper.getMainLooper())
+
+            while (serviceEnabled) {
+                // Toast send MainActivity
+                handlerMain.post {
+                    Toast.makeText(
+                        this@TimeService,
+                        SimpleDateFormat(
+                            "yyyy.MM.dd. HH:mm:ss",
+                            Locale.ROOT
+                        ).format(Date(System.currentTimeMillis())),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                sleep(5000)
+            }
+        }
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        serviceEnabled = true
+        TimeThread().start()
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // stop thread
+        serviceEnabled = false
+
+    }
+}
