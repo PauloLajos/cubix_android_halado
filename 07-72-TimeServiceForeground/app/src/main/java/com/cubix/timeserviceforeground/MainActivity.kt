6ee16122c12961958f9cc509699a015379a7fae2
paulolajos <dev@paulolajos.hu>
2023-07-31 12:39:39 +0200

    07. -72- TimeServiceForeground - complete

--- 07-72-TimeServiceForeground/app/src/main/java/com/cubix/timeserviceforeground/MainActivity.kt
+++ 07-72-TimeServiceForeground/app/src/main/java/com/cubix/timeserviceforeground/MainActivity.kt
@@ -4,10 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cubix.timeserviceforeground.databinding.ActivityMainBinding
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 1001
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.FOREGROUND_SERVICE)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +32,50 @@ class MainActivity : AppCompatActivity() {
             binding.btnStart.isEnabled = false
             binding.btnStop.isEnabled = true
         }
+
         binding.btnStop.setOnClickListener {
             stopService(intentTimeService)
             binding.btnStart.isEnabled = true
             binding.btnStop.isEnabled = false
         }
+
+        if (allPermissionsGranted()) {
+            setViews()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                setViews()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun setViews() {
+        binding.btnStart.isEnabled = true
+        binding.btnStop.isEnabled = false
     }
 }
--- 07-72-TimeServiceForeground/app/src/main/java/com/cubix/timeserviceforeground/TimeService.kt
+++ 07-72-TimeServiceForeground/app/src/main/java/com/cubix/timeserviceforeground/TimeService.kt
@@ -1,16 +1,26 @@
 package com.cubix.timeserviceforeground
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
 class TimeService : Service() {
+
+    private val NOTIFICATION_CHANNEL_ID = "time_service_notifications"
+    private val NOTIFICATION_CHANNEL_NAME = "Time Service notifications"
+    private val NOTIF_FOREGROUND_ID = 101
 
     var serviceEnabled = false
 
@@ -22,16 +32,23 @@ class TimeService : Service() {
 
             while (serviceEnabled) {
                 // Toast send MainActivity
+
+                val dateTime = SimpleDateFormat(
+                    "yyyy.MM.dd. HH:mm:ss",
+                    Locale.ROOT
+                ).format(
+                    Date(System.currentTimeMillis())
+                )
+
                 handlerMain.post {
                     Toast.makeText(
                         this@TimeService,
-                        SimpleDateFormat(
-                            "yyyy.MM.dd. HH:mm:ss",
-                            Locale.ROOT
-                        ).format(Date(System.currentTimeMillis())),
+                        dateTime,
                         Toast.LENGTH_SHORT
                     ).show()
                 }
+
+                updateNotification(dateTime)
 
                 sleep(5000)
             }
@@ -43,10 +60,52 @@ class TimeService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        startForeground(NOTIF_FOREGROUND_ID, getMyNotification("Hello foreground"))
         serviceEnabled = true
         TimeThread().start()
 
         return START_STICKY
+    }
+
+    private fun updateNotification(text: String) {
+        val notification = getMyNotification(text)
+        val notifMan = getSystemService(NOTIFICATION_SERVICE) as NotificationManager?
+        notifMan?.notify(NOTIF_FOREGROUND_ID, notification)
+    }
+
+    private fun getMyNotification(text: String): Notification {
+        createNotificationChannel()
+
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val contentIntent = PendingIntent.getActivity(
+            this,
+            NOTIF_FOREGROUND_ID,
+            notificationIntent,
+            PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        return NotificationCompat.Builder(
+            this, NOTIFICATION_CHANNEL_ID
+        )
+            .setContentTitle("This the MyTimeService")
+            .setContentText(text)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setVibrate(longArrayOf(1000, 2000, 1000))
+            .setContentIntent(contentIntent).build()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+        notificationManager?.createNotificationChannel(channel)
     }
 
     override fun onDestroy() {
@@ -54,6 +113,6 @@ class TimeService : Service() {
 
         // stop thread
         serviceEnabled = false
-
+        stopForeground(STOP_FOREGROUND_DETACH)
     }
 }
