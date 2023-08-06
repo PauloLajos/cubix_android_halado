05fd08496b7b8b446bfa3caa7ffbd64b7f3726c6
Pauló Lajos <dev@paulolajos.hu>
2023-08-06 08:47:05 +0200

    07. -78- ContentProviderDemo - getContacts - insertCalendarEvent

--- 07-78-ContentProviderDemo/app/src/main/java/com/cubix/contentproviderdemo/MainActivity.kt
+++ 07-78-ContentProviderDemo/app/src/main/java/com/cubix/contentproviderdemo/MainActivity.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import androidx.core.app.ActivityCompat
 import com.cubix.contentproviderdemo.databinding.ActivityMainBinding
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.pm.PackageManager
+import android.provider.CalendarContract
+import android.provider.ContactsContract
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import java.util.TimeZone
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +20,8 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val REQUEST_CODE_PERMISSIONS = 1001
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CALENDAR)
+        private val REQUIRED_PERMISSIONS =
+            arrayOf(Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CALENDAR)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -71,10 +77,48 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun insertCalendarEvent() {
-        TODO("Not yet implemented")
+        try {
+            val values = ContentValues()
+            values.put(CalendarContract.Events.DTSTART, System.currentTimeMillis())
+            values.put(CalendarContract.Events.DTEND, System.currentTimeMillis() + 60000)
+            values.put(CalendarContract.Events.TITLE, "Vége")
+            values.put(CalendarContract.Events.DESCRIPTION, "Legyen már vége a mostani órának :)")
+            values.put(CalendarContract.Events.CALENDAR_ID, 1)
+            values.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.getDefault().getID())
+
+            val uri = contentResolver.insert(CalendarContract.Events.CONTENT_URI, values)
+
+            //contentResolver.delete(CalendarContract.Events.CONTENT_URI, CalendarContract.Events._ID+"=599", null)
+
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
     }
 
+    @SuppressLint("Range")
     private fun getContacts() {
-        TODO("Not yet implemented")
+        val cursorContacts =
+            contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                arrayOf(
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                    ContactsContract.CommonDataKinds.Phone.NUMBER
+                ),
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " LIKE '%Tamas%'",
+                //null,
+                null,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " DESC"
+            )
+
+        Toast.makeText(this, "" + cursorContacts!!.count, Toast.LENGTH_LONG).show();
+
+        while (cursorContacts.moveToNext()) {
+            val name = cursorContacts.getString(
+                cursorContacts.getColumnIndex(
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+                )
+            )
+            Toast.makeText(this@MainActivity, name, Toast.LENGTH_LONG).show()
+        }
     }
 }
