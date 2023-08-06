8a121291c6d5357789e3fd10950ca1c5f762874f
Pauló Lajos <dev@paulolajos.hu>
2023-08-06 07:49:03 +0200

    07. -78- ContentProviderDemo - layout, permissions

--- 07-78-ContentProviderDemo/app/src/main/java/com/cubix/contentproviderdemo/MainActivity.kt
+++ 07-78-ContentProviderDemo/app/src/main/java/com/cubix/contentproviderdemo/MainActivity.kt
@@ -2,10 +2,79 @@ package com.cubix.contentproviderdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.cubix.contentproviderdemo.databinding.ActivityMainBinding
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 1001
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CALENDAR)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (allPermissionsGranted()) {
+            setViews()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                setViews()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun setViews() {
+        binding.btnGet.isEnabled = true
+        binding.btnInsert.isEnabled = true
+
+        binding.btnGet.setOnClickListener {
+            getContacts()
+        }
+        binding.btnInsert.setOnClickListener {
+            insertCalendarEvent()
+        }
+    }
+
+    private fun insertCalendarEvent() {
+        TODO("Not yet implemented")
+    }
+
+    private fun getContacts() {
+        TODO("Not yet implemented")
     }
 }
