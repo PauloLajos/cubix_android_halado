5460e488ad4d4ec764fea2a261d9725686fe9949
paulolajos <dev@paulolajos.hu>
2023-08-11 08:08:17 +0200

    08. -85- DataBindingDemo - image change

--- 08-85-DataBindingDemo/app/build.gradle.kts
+++ 08-85-DataBindingDemo/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    // databinding @BindingAdapter need kapt
+    id("org.jetbrains.kotlin.kapt")
 }
 
 android {
@@ -28,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         //noinspection DataBindingWithoutKapt
@@ -49,4 +51,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    ksp("com.github.bumptech.glide:ksp:4.14.2")
+
 }
--- 08-85-DataBindingDemo/app/src/main/java/hu/paulolajos/databindingdemo/MainActivity.kt
+++ 08-85-DataBindingDemo/app/src/main/java/hu/paulolajos/databindingdemo/MainActivity.kt
@@ -2,11 +2,15 @@ package hu.paulolajos.databindingdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import hu.paulolajos.databindingdemo.databinding.ActivityMainBinding
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -45,6 +49,31 @@ class MainActivity : AppCompatActivity() {
         // imput value
         binding.btnDemo.setOnClickListener {
             Toast.makeText(this, binding.cityName,Toast.LENGTH_LONG).show()
+        }
+
+        // image change
+        val rand = Random()
+        changeImage(rand)
+
+        binding.btnImage.setOnClickListener {
+            changeImage(rand)
+        }
+    }
+
+    private fun changeImage(rand: Random) {
+        binding.imageUrl = "://picsum.photos/200/200/?image=${rand.nextInt(1000)}"
+    }
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter(value = ["setImageUrl"])
+        fun ImageView.bindImageUrl(url: String?) {
+            if (!url.isNullOrBlank()) {
+                Glide.with(this.context)
+                    .load("https$url")
+                    .error("http$url")
+                    .into(this)
+            }
         }
     }
 }
