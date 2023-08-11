bd5c336498f8cefce2d5b432432ad2681f7c330c
paulolajos <dev@paulolajos.hu>
2023-08-11 06:21:02 +0200

    08. -85- DataBindingDemo - data & view binding

---
+++ 08-85-DataBindingDemo/app/src/main/java/hu/paulolajos/databindingdemo/MainActivity.kt
@@ -0,0 +1,20 @@
+package hu.paulolajos.databindingdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import hu.paulolajos.databindingdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    // Data binding !!!
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.dataBindingDemoString = "Hello data binding!"
+        binding.tvViewBinding.text = "Hello view binding!"
+    }
+}
