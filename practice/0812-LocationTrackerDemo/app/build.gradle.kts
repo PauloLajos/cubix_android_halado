5887128ac06765fb328fc027bcb421e159938d60
paulolajos <dev@paulolajos.hu>
2023-08-12 08:43:57 +0200

    Practice - LocationTrackerDemo - connect Firebase realtime database - not inserting data :(

--- practice/0812-LocationTrackerDemo/app/build.gradle.kts
+++ practice/0812-LocationTrackerDemo/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -56,4 +57,6 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
+
+    implementation("com.google.firebase:firebase-database-ktx:20.2.2")
 }
--- practice/0812-LocationTrackerDemo/app/src/main/java/hu/paulolajos/locationtrackerdemo/ui/fragments/MapFragment.kt
+++ practice/0812-LocationTrackerDemo/app/src/main/java/hu/paulolajos/locationtrackerdemo/ui/fragments/MapFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -12,9 +15,20 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import hu.paulolajos.locationtrackerdemo.R
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import hu.paulolajos.locationtrackerdemo.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
-    //
+
+    // view
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+
+    // map
     private lateinit var googleMap: GoogleMap
     private var currentPosition = LatLng(0.0, 0.0)
 
@@ -33,12 +47,98 @@ class MapFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        // Inflate the layout for this fragment
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+        setupLocClient()
+    }
+
+    private lateinit var fusedLocClient: FusedLocationProviderClient
+    // use it to request location updates and get the latest location
+
+    private fun setupLocClient() {
+        fusedLocClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+    }
+
+    // prompt the user to grant/deny access
+    private fun requestLocPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), //permission in the manifest
+            REQUEST_LOCATION)
+    }
+
+    private fun getCurrentLocation() {
+        // Check if the ACCESS_FINE_LOCATION permission was granted before requesting a location
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+        ) !=
+            PackageManager.PERMISSION_GRANTED) {
+
+            // call requestLocPermissions() if permission isn't granted
+            requestLocPermissions()
+
+        } else {
+            fusedLocClient.lastLocation.addOnCompleteListener {
+                // lastLocation is a task running in the background
+                val location = it.result //obtain location
+                //reference to the database
+                val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+                val ref: DatabaseReference = database.getReference("test")
+                if (location != null) {
+
+                    val latLng = LatLng(location.latitude, location.longitude)
+                    // create a marker at the exact location
+                    googleMap.addMarker(MarkerOptions().position(latLng)
+                        .title("You are currently here!"))
+                    // create an object that will specify how the camera will be updated
+                    val update = CameraUpdateFactory.newLatLngZoom(latLng, 16.0f)
+
+                    googleMap.moveCamera(update)
+                    //Save the location data to the database
+                    ref.setValue(location)
+                } else {
+                    // if location is null , log an error message
+                    Log.e(TAG, "No location found")
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        //check if the request code matches the REQUEST_LOCATION
+        if (requestCode == REQUEST_LOCATION)
+        {
+            //check if grantResults contains PERMISSION_GRANTED.If it does, call getCurrentLocation()
+            if (grantResults.size == 1 && grantResults[0] ==
+                PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation()
+            } else {
+                //if it doesn`t log an error message
+                Log.e(TAG, "Location permission has been denied")
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val REQUEST_LOCATION = 1 //request code to identify specific permission request
+        private const val TAG = "MapsActivity" // for debugging
     }
 }
