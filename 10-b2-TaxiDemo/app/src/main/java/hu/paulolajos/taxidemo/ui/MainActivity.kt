5ac548f79e8055818e949f7a93b28973f105fc45
paulolajos <dev@paulolajos.hu>
2023-08-15 08:08:20 +0200

    10. -112- Házi feladat - add google maps

--- 10-b2-TaxiDemo/app/src/main/java/hu/paulolajos/taxidemo/ui/MainActivity.kt
+++ 10-b2-TaxiDemo/app/src/main/java/hu/paulolajos/taxidemo/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package hu.paulolajos.taxidemo.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import hu.paulolajos.taxidemo.R
+import androidx.appcompat.app.AppCompatActivity
+import hu.paulolajos.taxidemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
