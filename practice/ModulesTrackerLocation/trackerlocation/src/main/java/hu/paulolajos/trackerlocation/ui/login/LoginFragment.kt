249d07a802389426cc10a54f444d900f3c92fe32
paulolajos <dev@paulolajos.hu>
2023-09-07 06:02:10 +0200

    Practice - Modules Tracker Location - add binding

--- practice/ModulesTrackerLocation/trackerlocation/src/main/java/hu/paulolajos/trackerlocation/ui/login/LoginFragment.kt
+++ practice/ModulesTrackerLocation/trackerlocation/src/main/java/hu/paulolajos/trackerlocation/ui/login/LoginFragment.kt
@@ -1,20 +1,23 @@
 package hu.paulolajos.trackerlocation.ui.login
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import hu.paulolajos.trackerlocation.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import hu.paulolajos.trackerlocation.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var viewModel: LoginViewModel
 
     companion object {
         fun newInstance() = LoginFragment()
     }
-
-    private lateinit var viewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +29,15 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
+    /**
+     * Frees the binding object when the Fragment is destroyed.
+     */
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
